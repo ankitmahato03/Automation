25d0953afdf36bc7b8afa5d13cf486f7d464507f
Ankit Mahato <ankitmahato1212@gmail.com>
2025-06-06 23:00:25 +0530

    2nd automation

---
+++ automation_two.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Folder to back up
+SOURCE_DIR="$HOME/Documents"
+
+# Backup destination
+DEST="$HOME/backup_$(date +%F).tar.gz"
+
+# Create backup
+tar -czf "$DEST" "$SOURCE_DIR"
+
+echo "Backup saved to $DEST"
+
